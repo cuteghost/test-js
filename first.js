dacbc9e0c74cd9ad4d18791ca8e9710212b42829
cuteghost <dzevad.alibegovic@edu.fit.ba>
2022-02-21 16:23:14 +0100

    updt2

--- first.js
+++ first.js
@@ -1,9 +1,10 @@
 import secondTest from "https://cdn.jsdelivr.net/gh/cuteghost/test-js/second.js"
 
 function first(){
-     secondTest();
+     $.getscript("https://cdn.jsdelivr.net/gh/cuteghost/test-js/second.js", function(){
+         secondTest();
+     });
 }
 function second(){
     alert("radi dvica");
   }
-  
